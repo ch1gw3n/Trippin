b5a181a97fd470fac1f4fa28370596e1b659b979
Chi Nguyen <nguyenct1@g.cofc.edu>
2024-04-05 10:52:41 -0400

    made header, random codes

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// components
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+// screen parts
+import trippinFooter from './components/trippinFooter';
+import trippinHeader from './components/trippinHeader';
+
+// screens ?
+
+const Drawer = createDrawerNavigator();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <NavigationContainer>
+        <View style={styles.container}>
+          <trippinHeader />
+          <Drawer.Navigator useLegacyImplementation initialRouteName="Welcome">
+            <Drawer.Screen name="Welcome" component={WelcomeScreen} />
+            <Drawer.Screen name="Login" component={LoginScreen} /> 
+            // we have some other screen
+          </Drawer.Navigator>
+        </View>
+        <View style={styles.footerContainer}>
+          <trippinFooter />
+        </View>
+      </NavigationContainer>
+    </>
   );
 }
 
@@ -17,4 +40,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  footerContainer: { backgroundColor: '#333333' },
 });
