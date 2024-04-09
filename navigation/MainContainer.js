24bbc0c0ef9e5a62d1039d5d9ce99f43526b128b
Chi Nguyen <nguyenct1@g.cofc.edu>
2024-04-09 15:32:05 -0400

    update

---
+++ navigation/MainContainer.js
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+// Screens
+import HomeScreen from './screens/HomeScreen';
+import TripsScreen from './screens/TripsScreen';
+import SettingsScreen from './screens/SettingsScreen';
+
+//Screen names
+const homeName = "Home";
+const tripsName = "Trips";
+const settingsName = "Settings";
+
+const Tab = createBottomTabNavigator();
+
+function MainContainer() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName={tripsName}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            let rn = route.name;
+
+            if (rn === homeName) {
+              iconName = focused ? 'home' : 'home-outline';
+
+            } else if (rn === tripsName) {
+              iconName = focused ? 'location' : 'location-outline';
+
+            } else if (rn === settingsName) {
+              iconName = focused ? 'settings' : 'settings-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarLabelStyle: {
+            paddingBottom: 10,
+            fontSize: 10
+            },
+          tabBarStyle: [{ display: 'flex', padding: 10, height: 90 }, null]
+        })}>
+
+        <Tab.Screen name={homeName} component={HomeScreen} />
+        <Tab.Screen name={tripsName} component={TripsScreen} />
+        <Tab.Screen name={settingsName} component={SettingsScreen} />
+
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default MainContainer;
--- navigation/screens/Extra.js
+++ navigation/screens/Extra.js
@@ -1,19 +1,12 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
 
-export default function Extra({navigation}) {
-    return(
-        <View style={{
-            flex: 1,
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}>
+export default function Extra({ navigation }) {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text
                 onPress={() => navigation.navigate('Home')}
-                style={{ 
-                    fontSize: 26,
-                    fontWeight: 'bold'
-                }}>Extra</Text>
+                style={{ fontSize: 26, fontWeight: 'bold' }}>Settings Screen</Text>
         </View>
-    )
+    );
 }
