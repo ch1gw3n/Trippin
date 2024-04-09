068496da4ca7db1e9d3413d781bfccaeff8a0b50
Chi Nguyen <nguyenct1@g.cofc.edu>
2024-04-09 00:34:24 -0400

    working ish

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { StyleSheet } from 'react-native'; // Import StyleSheet from react-native
 import Main from './navigation/Main';
 
 export default function App() {
@@ -6,7 +7,6 @@ export default function App() {
      <Main/>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
