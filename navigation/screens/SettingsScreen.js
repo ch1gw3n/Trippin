147f177fa04647853f2d4b0e6a41a7cfd8788369
Chi Nguyen <nguyenct1@g.cofc.edu>
2024-04-21 12:28:37 -0400

    settings

--- navigation/screens/SettingsScreen.js
+++ navigation/screens/SettingsScreen.js
@@ -3,10 +3,60 @@ import { View, Text } from 'react-native';
 
 export default function SettingsScreen({ navigation }) {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#DCCFEC' }}>
-            <Text
+        <View style={styles.containerStyle}>
+            <View style= {styles.infoWrapperStyle}>
+                <Text
                 onPress={() => navigation.navigate('Home')}
                 style={{ fontSize: 26, fontWeight: 'bold' }}>Settings Screen</Text>
+                <Text style={styles.title}>Developed by:</Text>
+                <Text style={styles.name}>Chi Nguyen & Erika Roberts</Text>
+                <Text style={styles.description}>Sprinh 2024</Text>
+                <Text style={styles.description}>College of Charleston</Text>
+            </View>
         </View>
+        
     );
 }
+
+let styles = StyleSheet.create({
+  containerStyle: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    backgroundColor: "#eeeeee",
+  },
+  infoWrapperStyle: {
+    width: "70%",
+    textAlign: "left",
+    borderRadius: 10,
+    paddingVertical: 10,
+    elevation: 5,
+    paddingHorizontal: 30,
+    marginBottom: 50,
+    backgroundColor: "#ffffff",
+  },
+  title: {
+    fontWeight: "bold",
+    textAlign: "right",
+    fontFamily: "serif",
+    marginVertical: 5,
+  },
+  name: {
+    fontWeight: "bold",
+    textAlign: "right",
+    marginVertical: 8,
+    fontFamily: "serif",
+    fontSize: 20,
+  },
+  description: {
+    marginVertical: 2,
+    textAlign: "right",
+  },
+  iconsWrapper: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "60%",
+    alignItems: "center",
+    marginBottom: 40,
+  }
+});
